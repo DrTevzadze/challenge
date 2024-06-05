2cffd97d7f57d974c556b3152d1c729f447e33db
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-06-05 21:25:13 +0400

    adjust timing for welcome delay animation

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -4,29 +4,23 @@ import { useNavigate } from "react-router-dom";
 function Welcome() {
   const navigate = useNavigate();
   const [isVisible, setIsVisible] = useState(true);
-  const [showH1, setShowH1] = useState(false);
-  const [showH2, setShowH2] = useState(false);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
-    const h1Timer = setTimeout(() => {
-      setShowH1(true);
-    }, 100);
-
-    const h2Timer = setTimeout(() => {
-      setShowH2(true);
-    }, 1000);
+    const timer = setTimeout(() => {
+      setShow(true);
+    }, 300);
 
     const fadeOutTimer = setTimeout(() => {
       setIsVisible(false);
-    }, 3000);
+    }, 4000);
 
     const navigationTimer = setTimeout(() => {
       navigate("/home");
-    }, 4000);
+    }, 5000);
 
     return () => {
-      clearTimeout(h1Timer);
-      clearTimeout(h2Timer);
+      clearTimeout(timer);
       clearTimeout(fadeOutTimer);
       clearTimeout(navigationTimer);
     };
@@ -40,15 +34,15 @@ function Welcome() {
     >
       <div className="flex flex-col items-center">
         <h1
-          className={`text-5xl text-white mb-4 transition-all duration-1000 transform ${
-            showH1 ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"
+          className={`text-5xl text-white mb-4  transition-all duration-1000 transform animate-pulse ${
+            show ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"
           }`}
         >
           Welcome to the Settlement App!
         </h1>
         <h2
-          className={`text-3xl text-white mb-4 transition-all duration-1000 transform ${
-            showH2 ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"
+          className={`text-3xl text-white mb-4  transition-all duration-1000 transform animate-pulse ${
+            show ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"
           }`}
         >
           I hope you will enjoy the final result.
