c9fc6492489e11cf8f4320777fcc1802e33d78c7
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-06-06 11:35:34 +0400

    Update welcome text and typescript for Homepage.tsx currentview state

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import PartyA from "../components/PartyA";
 import PartyB from "../components/PartyB";
 
 function Homepage() {
-  const [currentView, setCurrentView] = useState("PartyA");
+  const [currentView, setCurrentView] = useState<"PartyA" | "PartyB">("PartyA");
 
   const toggleView = () => {
     setCurrentView(currentView === "PartyA" ? "PartyB" : "PartyA");
@@ -12,7 +12,9 @@ function Homepage() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <header className="flex justify-between items-center bg-white shadow-md p-4">
-        <h1 className="text-4xl font-bold text-blue-500">Settlement Demo</h1>
+        <h1 className="text-lg font-bold text-blue-500 xl:text-4xl sm:text-2xl">
+          Settlement Demo
+        </h1>
         <button
           onClick={toggleView}
           className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-600 transition-all duration-200"
