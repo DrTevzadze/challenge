77002c3f0bf5cd16dd58e3621eddbae4ce2c458c
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-06-10 01:03:01 +0400

    remove partya partyb titles. adjust margin for addformbutton and make formcard responsive

--- src/components/PartyB.tsx
+++ src/components/PartyB.tsx
@@ -15,9 +15,6 @@ const PartyB: React.FC = () => {
 
   return (
     <div className="container mx-auto flex flex-col items-center min-h-screen">
-      <h2 className="text-2xl font-semibold text-blue-600 text-center mb-6">
-        Party B View
-      </h2>
       <button
         onClick={handleShowForm}
         className="bg-blue-600 text-white font-bold px-6 py-3 rounded-md shadow-md hover:bg-blue-700 transition-all duration-200"
--- src/components/formList/AddFormButton.tsx
+++ src/components/formList/AddFormButton.tsx
@@ -1,4 +1,5 @@
 import AddFormSVGIcon from "./AddFormSVGIcon";
+
 interface FormButtonProps {
   onClick: () => void;
 }
@@ -6,7 +7,7 @@ interface FormButtonProps {
 const AddFormButton: React.FC<FormButtonProps> = ({ onClick }) => {
   return (
     <div
-      className="inline-flex items-center font-bold bg-blue-500 w-auto text-white py-4 px-6 m-4 rounded-md cursor-pointer hover:bg-blue-600 transition-all duration-200"
+      className="inline-flex items-center font-bold bg-blue-500 w-auto text-white py-4 px-6 m-2 md:m-4 rounded-md cursor-pointer hover:bg-blue-600 transition-all duration-200"
       onClick={onClick}
     >
       <AddFormSVGIcon />
